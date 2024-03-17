0a4b8fd5427bc50d8e2a2d80087a37435e20acf5
muttyeb2003 <muttyeb2003@gmail.com>
2024-03-17 00:56:29 -0300

    Added buttons
    Added the learn more and read description button

---
+++ Home_Page/home.js
@@ -0,0 +1,13 @@
+// Function to toggle the visibility of the expanded content
+function toggleExpandedContent(contentId) {
+  let expandedContent = document.getElementById(contentId);
+  if (expandedContent.style.display === "block") {
+    expandedContent.style.display = "none";
+  } else {
+    expandedContent.style.display = "block";
+  }
+}
+function readDescription() {
+  var descriptionText = document.getElementById("expandedContent").innerText;
+  alert(descriptionText);
+}
