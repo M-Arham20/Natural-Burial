ccbe26063035b7d791bf1944bf81f750a4415d63
muttyeb2003 <muttyeb2003@gmail.com>
2024-03-19 19:08:13 -0300

    dd

---
+++ Premium_Subscription/PSubscription.js
@@ -0,0 +1,9 @@
+// Function to toggle the visibility of the expanded content
+function toggleExpandedContent(contentId) {
+  let expandedContent = document.getElementById(contentId);
+  if (expandedContent.style.display === "block") {
+    expandedContent.style.display = "none";
+  } else {
+    expandedContent.style.display = "block";
+  }
+}
