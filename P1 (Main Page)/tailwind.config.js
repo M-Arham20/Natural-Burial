686fcc7c6d644b90f27c496dc0201fbfa8365bf0
muttyeb2003 <muttyeb2003@gmail.com>
2024-04-01 23:25:53 -0300

    Midway done with tailwind
    Midway saloon (Subhan and Asfand) koi kaam kerlo

---
+++ P1 (Main Page)/tailwind.config.js
@@ -0,0 +1,16 @@
+// // tailwind.config.js
+
+// module.exports = {
+//     theme: {
+//       extend: {
+//         colors: {
+//           khaki: {
+//             DEFAULT: '#F0E68C',
+//           },
+//         },
+//       },
+//     },
+//     variants: {},
+//     plugins: [],
+//   }
+  
